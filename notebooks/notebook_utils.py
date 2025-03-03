7bad37020a524b22f661a7882ae57d1ff1f48ee3
clara-levt <121702804+clara-levt@users.noreply.github.com>
2025-03-03 14:48:31 +0700

    Update notebook_utils.py

--- notebooks/notebook_utils.py
+++ notebooks/notebook_utils.py
@@ -19,10 +19,28 @@ INTERFACEGAN_PATHS = {
     "pose": {'id': '1nCzCR17uaMFhAjcg6kFyKnCCxAKOCT2d', 'name': 'pose_boundary.npy'},
     "Male": {'id': '18dpXS5j1h54Y3ah5HaUpT03y58Ze2YEY', 'name': 'Male_boundary.npy'},
     # Adding interFace GAN+++ boudaries
-    "oval_face": {'id': '1CgRjliZJxVBSPnjtMrc2yjp2Ism14Gp6', 'name': 'boundary_Oval_Face.npy'},
-    "big_nose": {'id': '1AjQ9Lo3GGIJFf0r-l_Ip8AdshKcFbgRA', 'name': 'boundary_Big_Nose.npy'},
-    "big_lips": {'id':'1Sp1-gv7VN3kJfTKRxbbVLg1u3Vn_dSZ-','name':'boundary_Big_Lips.npy'}
+    "oval_face": {'id': '102zfQ_2Rv9RVTdukMi3xMuqBTp3i70YK', 'name': 'Oval_Face_boundary.npy'},
+    "big_nose": {'id': '1lz_nbsfzUXywzp1v70uGoPLtPwlqg8hA', 'name': 'Big_Nose_boundary.npy'},
+    "big_lips": {'id':'10C2DZ3Nsv9n3mAyXEbb1mea-XY06pKcL','name':'Big_Lips_boundary.npy'},
+    "pointy_nose": {'id': '1amS402iHbNci9lc9ThsH65a1ateiWwd5','name': 'Pointy_Nose_boundary.npy'},
+    "attractive": {'id': '1WL5mc5tDkbP0fD2U2iF1hEfmx7-8K4PH','name':'Attractive_boundary.npy'},
+    'straight_hair': {'id':'1BrTPXndDi4wChBJ-bfcPo4Vcs2YE5ELE', 'name':'Straight_Hair_boundary.npy'},
+    'blond_hair': {'id':'1iQiJtbptxfnKIRFA5wwfTEZvzIVz4acY','name':'Blond_Hair_boundary.npy'},
+    'black_hair':{'id':'1f-fZlpV_x80gc_dVJP1oRDg-AqU_no-N', 'name': 'Black_Hair_boundary.npy'},
+    'brown_hair':{'id':'1mrMvRZ_Rzy0iFcLIK8gKmwdoOlygg7-6','name':'Brown_Hair_boundary.npy'},
+    'wear_lipstick': {'id':'1zoytQM_kpUVj-BjPX35P8fSWNjzxWZq5','name':'Wearing_Lipstick_boundary.npy'},
+    'narrow_eyes': {'id':'12T8ChIIGDbxKeLEkVJG6D6a39SNXqw9s','name':'Narrow_Eyes_boundary.npy'},
+    'mouth_sligtly_open': {'id':'1G2-XDjuQvgZQwzxozSzOV2ftlrsMmptr','name':'Mouth_Slightly_Open_boundary.npy'},
+    'pale_skin': {'id':'1h-18I4IPqZmRId78YqZ8tnCC1b96Dr7t','name':'Pale_Skin_boundary.npy'},
+    'rossy_cheeks': {'id':'1akKTNzbB35RgKUofas-rsTrHiOXC0gx4','name':'Rosy_Cheeks_boundary.npy'},
+    'wavy_hair': {'id':'13E8bRbGMooQCRyoThBXtORiBh9eY1Dpn','name': 'Wavy_Hair_boundary.npy'},
+    'arched_eyebrows': {'id':'11q9-UjiZuyHGtBavTC_l_LEfpGBdKh70','name':'Arched_Eyebrows_boundary.npy'},
+    'chubby': {'id':'14bYK5Z2buYFehFBplKLAT1UKFdA9mNP0','name':'Chubby_boundary.npy'},
+    'double_chin': {'id':'1sXoSMgrHiSU8YEBW9-Pgy7bKOyA4RzgG','name':'Double_Chin_boundary.npy'},
+    'high_cheekbones': {'id':'1E2UqQyomIB_aGlk99Q8WpRZe9hv2zW6V','name':'High_Cheekbones_boundary.npy'},
+    'bushy_eyebrows': {'id':'1kC575Fn_co8zwf9fWgyOi-lyKO6dZwum','name':'Bushy_Eyebrows_boundary.npy'},
 }
+
 STYLECLIP_PATHS = {
     "delta_i_c": {"id": "1HOUGvtumLFwjbwOZrTbIloAwBBzs2NBN", "name": "delta_i_c.npy"},
     "s_stats": {"id": "1FVm_Eh7qmlykpnSBN1Iy533e_A2xM78z", "name": "s_stats"},
